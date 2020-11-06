4717a36f45d38cfe818f6908f9d4f09d306675d3
lavieth <68887484+lavieth@users.noreply.github.com>
2020-11-05 21:50:04 -0600

    Update TWI_funcs.h

--- TWI_funcs.h
+++ TWI_funcs.h
@@ -1,1 +1,10 @@
+#ifndef TWI_FUNCS_H
+#define TWI_FUNCS_H
+#include "board.h"
 
+
+uint8_t TWI_Master_Init(uint8_t volatile *TWI_addr, uint32_t I2C_freq);
+
+uint8_t TWI_Master_Receive(uint8_t volatile *TWI_addr, uint8_t device_addr, uint32_t int_addr, uint8_t int_addr_sz, uint16_t num_bytes, uint8_t * array_name);
+
+#endif

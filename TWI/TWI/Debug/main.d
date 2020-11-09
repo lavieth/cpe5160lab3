94ec62f5cb1b3e3e3f6146a94a4992463a9158a0
Sam Short <56653108+snspzv@users.noreply.github.com>
2020-11-08 22:54:28 -0600

    Revert "Added TWI_receive debugging function"

--- TWI/TWI/Debug/main.d
+++ TWI/TWI/Debug/main.d
@@ -10,8 +10,7 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
- .././TWI_funcs.h .././board.h .././STA013_funcs.h .././UART.h \
- .././Control_Outputs.h
+ .././TWI_funcs.h .././board.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -38,9 +37,3 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 .././TWI_funcs.h:
 
 .././board.h:
-
-.././STA013_funcs.h:
-
-.././UART.h:
-
-.././Control_Outputs.h:

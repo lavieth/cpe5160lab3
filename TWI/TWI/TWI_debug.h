d2da68a7ca389846e2cdf3017014a6c435e61a2b
lavieth <68887484+lavieth@users.noreply.github.com>
2020-11-10 22:25:16 -0600

    Update TWI_debug.h

--- TWI/TWI/TWI_debug.h
+++ TWI/TWI/TWI_debug.h
@@ -1,6 +1,15 @@
 #ifndef TWI_DEBUG_H
 #define TWI_DEBUG_H
+
+#include <stdio.h>
+#include "TWI_funcs.h"
+#include "board.h"
+#include "UART_Print.h"
+
+//takes in TWI_Master_Receive() and checks that it is working
 void receive_debug();
+
+void transmit_debug();
 
 
 

16d20b9ddc449d8c968d5d03180dfd6c348b0d4e
lavieth <68887484+lavieth@users.noreply.github.com>
2020-11-10 22:41:19 -0600

    Update TWI_debug.c

--- TWI/TWI/TWI_debug.c
+++ TWI/TWI/TWI_debug.c
@@ -1,23 +1,33 @@
-#include <stdio.h>
 #include "TWI_debug.h"
-#include "TWI_funcs.h"
-#include "board.h"
-#include "UART_Print.h"
-void receive_debug()
+
+void transmit_receive_debug()
 {
   uint8_t timeout_value = 10;
   uint8_t index=timeout_value;
   uint8_t error_status;
   uint8_t prnt_buffer [2];
-  //should return 0xAC, try 10 times
-  do
+  //TODO FOR TESTING, TRY JUST RECEIVE FIRST, THEN UNCOMMENT TRANSMIT BLOCK
+  //transmit sets internal addr to 1, receive reads the value and it should be
+  //0xAC 
+  /*do
   {
-    error_status = TWI_Master_Receive(&TWI1, 0x43, 0 ,0 ,3,array_name);
-    i--;
-  }while((error_status!=no_errors)&&(i!=0));
-  //print to verify
-  sprintf(prnt_buffer, "received value = %2.2bX\n\r", array_name[2]);
-  UART_Transmit_String(&UART1,0,prnt_buffer);
+    error_status=TWI_Master_Transmit(0x43,0x01,1,0,array_name);
+    index--;
+  }while((error_status!=no_errors)&&(index!=0));*/
   
-  return;
+  //print to verify
+   do
+  {
+     //this is for first try, with receive by itself
+     error_status = TWI_Master_Receive(&TWI1, 0x43, 0 ,0 ,3,array_name);
+     //for try with transmit running first
+     //error_status = TWI_Master_Receive(&TWI1, 0x43, 0 ,0 ,1,array_name);
+     index--;
+  }while((error_status!=no_errors)&&(index!=0));
+  
+  sprintf(prnt_buffer, "received value = %2.2bX\n\r", array_name[2]);
+  UART_Transmit_String(&UART1,0,prnt_buffer); 
+
 }
+
+
